7f46e62e2c5ff52d415df0591feb79df7b4057fb
Parker092 <53531818+Parker092@users.noreply.github.com>
2024-06-18 14:11:00 -0600

    navSet

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,11 @@ import Navbar from '../components/Navbar';
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-400 flex flex-col items-center text-white p-4">
-      <Navbar />
       <h1 className="text-4xl font-bold mb-6 text-center">Bienvenido al Dashboard del Clima de El Salvador</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-screen-lg mx-auto">
         {departments.map((department) => (
           <Link key={department.path} href={`/dashboard${department.path}`} legacyBehavior>
-            <a className="block p-4 bg-gray-800 rounded shadow hover:bg-gray-700 transition text-center">
+            <a className="block p-4 bg-gray-800 rounded shadow hover:bg-gray-200 transition text-center">
               {department.name.replace(/-/g, ' ')}
             </a>
           </Link>
